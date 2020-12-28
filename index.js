7a40be63f28f3f8e18bee5ed6d80ec763dcaee84
Jukka Aho <ahojukka5@gmail.com>
2020-12-28 21:01:03 +0700

    Change default port from 3002 to 3000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.NODE_PORT | 3002;
+const port = process.env.NODE_PORT | 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
