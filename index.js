681bbeadc2ac75e222da4ab7ae07878abf38942c
Jukka Aho <ahojukka5@gmail.com>
2021-01-02 01:44:09 +0700

    Do not read port from NODE_PORT

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT | process.env.NODE_PORT | 3000;
+const port = process.env.PORT | 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at port ${port}`);
 });
